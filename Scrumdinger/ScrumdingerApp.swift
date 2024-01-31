b2705f6873ba89737cc5ce8cd9b77c320b7a5b44
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-01-31 21:16:48 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Nicola Kaleta on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
