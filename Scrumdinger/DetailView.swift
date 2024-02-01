5791e19d1bc3bca6810819f6a7415e725a39c89b
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-01 11:28:00 +0100

    Navigate between screens ✅

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -13,9 +13,13 @@ struct DetailView: View {
     var body: some View {
         List {
             Section(header: Text("Meeting info")) {
-                Label("Start Meeting", systemImage: "timer")
-                    .font(.headline)
-                    .foregroundColor(.accentColor)
+                NavigationLink(destination: MeetingView()){
+                    
+                    
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
                 HStack {
                     Label("Length",  systemImage: "clock")
                     Spacer()
@@ -40,6 +44,7 @@ struct DetailView: View {
                 }
             }
         }
+        .navigationTitle(scrum.title)
     }
 }
 
