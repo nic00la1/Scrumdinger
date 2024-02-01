cef5cffa365c20314631ba8d5abf6b1fe0c70290
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-01 11:00:20 +0100

    Create the detail view ✅

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,22 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Nicola Kaleta on 01/02/2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        DetailView(scrum: DailyScrum.sampleData[0])
+    }
+}
