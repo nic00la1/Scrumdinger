3a610a10865149a30b1516174c5b5495a3a5de9f
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-01 13:04:00 +0100

    Add a theme view ✅

---
+++ Scrumdinger/ThemeView.swift
@@ -0,0 +1,25 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Nicola Kaleta on 01/02/2024.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    
+    var body: some View {
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: .infinity)
+            .background(theme.mainColor)
+            .foregroundColor(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+#Preview {
+    ThemeView(theme: .buttercup)
+}
