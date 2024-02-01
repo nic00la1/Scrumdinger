942313c37ac47500bdb348c5593c1aa1b03f2011
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-01 11:23:39 +0100

    Iterate through attendees ✅

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -34,6 +34,11 @@ struct DetailView: View {
                 }
                 .accessibilityElement(children: .combine)
             }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
         }
     }
 }
