de496b503af7e4353b187045a819be38195e4164
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-01 10:13:21 +0100

    Display a list of daily scrums ✅

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,23 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Nicola Kaleta on 01/02/2024.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums, id: \.title) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
