6ad9ad9a28b9c1844728565df6fb2523cf2d3e2d
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-02 19:15:28 +0100

    Fixed a bug that doesn't showed speakers properly ✅

--- Scrumdinger/Views/MeetingFooterView.swift
+++ Scrumdinger/Views/MeetingFooterView.swift
@@ -1,37 +1,28 @@
-//
-//  MeetingFooterView.swift
-//  Scrumdinger
-//
-//  Created by Nicola Kaleta on 02/02/2024.
-//
-
 import SwiftUI
+
 
 struct MeetingFooterView: View {
     let speakers: [ScrumTimer.Speaker]
-    var skipAction: () -> Void
+    var skipAction: ()->Void
     
     private var speakerNumber: Int? {
-        guard let index = speakers.firstIndex(where: { !$0.isCompleted}) else {return nil}
+        guard let index = speakers.firstIndex(where: { !$0.isCompleted }) else { return nil }
         return index + 1
     }
-    
     private var isLastSpeaker: Bool {
         return speakers.dropLast().allSatisfy { $0.isCompleted }
     }
-    
     private var speakerText: String {
-        guard let speakerNumber = speakerNumber else { return "No more speakers"}
+        guard let speakerNumber = speakerNumber else { return "No more speakers" }
         return "Speaker \(speakerNumber) of \(speakers.count)"
     }
-    
     var body: some View {
         VStack {
             HStack {
                 if isLastSpeaker {
                     Text("Last Speaker")
-                } else  {
-                    Text("Speaker 1 of 3")
+                } else {
+                    Text(speakerText)
                     Spacer()
                     Button(action: skipAction) {
                         Image(systemName: "forward.fill")
@@ -43,6 +34,8 @@ struct MeetingFooterView: View {
         .padding([.bottom, .horizontal])
     }
 }
+
+
 
 #Preview {
     MeetingFooterView(speakers: DailyScrum.sampleData[0].attendees.speakers, skipAction: {})
