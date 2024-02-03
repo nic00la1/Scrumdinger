690ffd266c34d7fd7cc86d271b7f50946d997fc7
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-03 23:29:37 +0100

    Save data in inactive state ✅

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -13,15 +13,24 @@ struct ScrumdingerApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ScrumsView(scrums: $store.scrums)
-                .task {
+            ScrumsView(scrums: $store.scrums) {
+                Task {
                     do {
-                        try await store.load()
-                    }
-                    catch {
+                        try await store.save(scrums: store.scrums)
+                    } catch {
                         fatalError(error.localizedDescription)
                     }
+                    
                 }
+            }
+            .task {
+                do {
+                    try await store.load()
+                }
+                catch {
+                    fatalError(error.localizedDescription)
+                }
+            }
         }
     }
 }
