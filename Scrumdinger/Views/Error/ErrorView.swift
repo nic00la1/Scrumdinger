76ad3a810fdf626d9ba43b6112416c717e9c871c
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-04 14:01:03 +0100

    Create an error view ✨✅

---
+++ Scrumdinger/Views/Error/ErrorView.swift
@@ -0,0 +1,45 @@
+//
+//  ErrorView.swift
+//  Scrumdinger
+//
+//  Created by Nicola Kaleta on 04/02/2024.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    let errorWraper: ErrorWrapper
+    
+    var body: some View {
+        VStack {
+            Text("An error has occured!")
+                .font(.title)
+                .padding(.bottom)
+            Text(errorWraper.error.localizedDescription)
+                .font(.headline)
+            Text(errorWraper.guidance)
+                .font(.caption)
+                .padding(.top)
+            Spacer()
+        }
+        .padding()
+        .background(.ultraThinMaterial)
+        .cornerRadius(16)
+    }
+}
+
+
+struct ErrorView_Previews: PreviewProvider {
+    enum SampleError: Error {
+        case errorRequired
+    }
+    
+    static var wrapper: ErrorWrapper {
+        ErrorWrapper(error: SampleError.errorRequired,
+                     guidance: "You can safely ignore this error.")
+    }
+    
+    static var previews: some View {
+        ErrorView(errorWraper: wrapper)
+    }
+}
